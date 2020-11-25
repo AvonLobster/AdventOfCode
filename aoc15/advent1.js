a24046a3123a6ae3de190c4f6b76fa2464a5ebd6
Geoff Carlson <carlson.geoff@gmail.com>
2020-11-25 15:39:23 -0500

    15 day 1 part 2

--- aoc15/advent1.js
+++ aoc15/advent1.js
@@ -3,6 +3,7 @@ function init1() {
     var content = '<h2>--- Day 1: Not Quite Lisp ---</h2>';
     content += '<input type="text" id="input1"></input><br>';
     content += '<button type="button" onclick="day1part1()">Part 1</button>';
+    content += '<button type="button" onclick="day1part2()">Part 2</button>';
     content += '<div id="output1"></div>';
     ac.innerHTML = content;
 }
@@ -17,4 +18,22 @@ function day1part1() {
     }
 
     document.getElementById("output1").innerHTML = floor;
+}
+
+function day1part2() {
+    var input = document.getElementById("input1").value;
+    var floor = 0;
+    var pos = 0;
+
+    for (var i = 0; i < input.length; i++) {
+        if (input.charAt(i) == '(') floor++;
+        else if (input.charAt(i) == ')') floor--;
+
+        if (floor == -1) {
+            pos = i + 1;
+            break;
+        }
+    }
+
+    document.getElementById("output1").innerHTML = pos;
 }
