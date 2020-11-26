7f4ce6fab85ee2d16d7b57fe1f7dbd559afe885d
Geoff Carlson <carlson.geoff@gmail.com>
2020-11-26 09:36:59 -0500

    15 day 5 part 1

--- aoc15/advent5.js
+++ aoc15/advent5.js
@@ -1,5 +1,36 @@
 function init5() {
     var ac = document.getElementById("adventContent");
-    var content = "<h2>5</h2>";
+    var content = "<h2>--- Day 5: Doeesn't He Have Intern-Elves For This? ---</h2>";
+    content += '<textarea id="input5"></textarea><br>';
+    content += '<button type="button" onclick="day5part1()">Part 1</button>';
+    content += '<button type="button" onclick="day5part2()">Part 2</button>';
+    content += '<div id="output5"></div>';
     ac.innerHTML = content;
+}
+
+function day5part1() {
+    let input = document.getElementById("input5").value;
+    let lines = input.split('\n');
+    let nice = 0;
+
+    for (let i = 0; i < lines.length; i++) {
+        if (hasAtLeast3Vowels(lines[i]) &&
+            hasDupLetters(lines[i]) &&
+            !lines[i].includes("ab") &&
+            !lines[i].includes("cd") &&
+            !lines[i].includes("pq") &&
+            !lines[i].includes("xy")) {
+            nice++;
+        }
+    }
+
+    document.getElementById("output5").innerHTML = nice;
+}
+
+function hasAtLeast3Vowels(line) {
+    return line.match(/[aeiou].*[aeiou].*[aeiou]/) != null;
+}
+
+function hasDupLetters(line) {
+    return line.match(/(.)\1/) != null;
 }
