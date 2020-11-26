19f4e42610b2c03b3147e881a56f29460fbd8070
Geoff Carlson <carlson.geoff@gmail.com>
2020-11-26 08:03:07 -0500

    15 day 3 part 1

--- aoc15/advent3.js
+++ aoc15/advent3.js
@@ -1,5 +1,43 @@
 function init3() {
     var ac = document.getElementById("adventContent");
-    var content = "<h2>3</h2>";
+    var content = "<h2>--- Day 3: Perfectly Sphereical Houses in a Vacuum ---</h2>";
+    content += '<input type="text" id="input3"></input><br>';
+    content += '<button type="button" onclick="day3part1()">Part 1</button>';
+    content += '<div id="output3"></div>';
     ac.innerHTML = content;
+}
+
+function day3part1() {
+    let input = document.getElementById("input3").value;
+    let houses = new Set();
+    let x =0;
+    let y = 0;
+    houses.add(coordToStr(x,y));
+
+    for (let i = 0; i < input.length; i++) {
+        let inst = input.charAt(i);
+
+        switch (inst) {
+            case "^": 
+                y++;    
+                break;
+            case "v": 
+                y--;
+                break;
+            case ">": 
+                x++;
+                break;
+            case "<": 
+                x--;
+                break;
+        }
+
+        houses.add(coordToStr(x,y));
+    }
+
+    document.getElementById("output3").innerHTML = houses.size;
+}
+
+function coordToStr(x, y) {
+    return x + "," + y;
 }
