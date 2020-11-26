656dbf99830c9ad93e6c710c8b9497bbfede1917
Geoff Carlson <carlson.geoff@gmail.com>
2020-11-26 08:44:50 -0500

    15 day 4 part 1

--- aoc15/advent4.js
+++ aoc15/advent4.js
@@ -1,5 +1,26 @@
 function init4() {
     var ac = document.getElementById("adventContent");
-    var content = "<h2>4</h2>";
+    var content = "<h2>--- Day 4: The Ideal Stocking Stuffer ---</h2>";
+    content += '<input type="text" id="input4"></input><br>';
+    content += '<button type="button" onclick="day4part1()">Part 1</button>';
+    content += '<div id="output4"></div>';
     ac.innerHTML = content;
+}
+
+function day4part1() {
+    let input = document.getElementById("input4").value;
+    let c = 1;
+    let done = false;
+
+    while (!done) {
+        let hash = md5(input + c).toString();
+
+        if (hash.startsWith("00000")) {
+            done = true;
+        }
+
+        c++;
+    }
+
+    document.getElementById("output4").innerHTML = --c;
 }
