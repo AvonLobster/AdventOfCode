d191a72f3aced78bc43456836a3923b9689e1603
Geoff Carlson <carlson.geoff@gmail.com>
2020-12-02 00:41:18 -0500

    2020Day2Part1

--- advent2.js
+++ advent2.js
@@ -1,5 +1,35 @@
 function init2() {
     var ac = document.getElementById("adventContent");
-    var content = "<h2>2</h2>";
+    var content = "<h2>--- Day 2: Password Philosophy ---</h2>";
+    content += '<textarea id="input2"></textarea><br>';
+    content += '<button type="button" onclick="day2part1()">Part 1</button>';
+    content += '<button type="button" onclick="day2part2()">Part 2</button>';
+    content += '<div id="output2"></div><br>';
     ac.innerHTML = content;
+}
+
+function day2part1() {
+    let input = document.getElementById("input2").value;
+    let lines = input.split("\n");
+    let validCount = 0;
+    
+    for (let i = 0; i < lines.length; i++) {
+        let min = parseInt(lines[i].match(/(\d+)-/));
+        let max = parseInt(lines[i].match(/\d+(?!.*-)/));
+        let ch = lines[i].charAt(lines[i].indexOf(":") - 1);
+        let pw = lines[i].substring(lines[i].indexOf(":") + 2);
+        let count = pw.split(ch).length - 1;
+        
+        if (count >= min && count <= max) validCount++;
+    }
+
+    document.getElementById("output2").innerHTML = validCount;
+}
+
+function day2part2() {
+    let input = document.getElementById("input2").value;
+
+
+
+    document.getElementById("output2").innerHTML = ""
 }
--- aoc15/advent6.js
+++ aoc15/advent6.js
@@ -1,5 +1,51 @@
 function init6() {
     var ac = document.getElementById("adventContent");
-    var content = "<h2>6</h2>";
+    var content = "<h2>--- Day 6: Probably a Fire Hazard---</h2>";
+    content += '<textarea id="input6"></textarea><br>';
+    content += '<button type="button" onclick="day6part1()">Part 1</button>';
+    content += '<button type="button" onclick="day6part2()">Part 2</button>';
+    content += '<div id="output6"></div><br>';
+    content += '<div id="visual6"></div>';
     ac.innerHTML = content;
+}
+
+function day6part1() {
+    let grid = buildGrid();
+    let input = document.getElementById("input6").value;
+    let instructions = input.split("\n");
+
+    for (let i = 0; i < instructions.length; i++) {
+        let xStart = 0;
+        let yStart = 0;
+        let xEnd = 0;
+        let yEnd = 0;
+    }
+
+    printVisualAndAnswer(grid);
+}
+
+function buildGrid() {
+    let grid = new Array(999).fill("0");
+
+    for (let i = 0; i < grid.length; i++) {
+        grid[i] = new Array(999).fill("0");
+    }
+
+    return grid;
+}
+
+function printVisualAndAnswer(grid) {
+    let content = "";
+    let onCount = 0;
+
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            content += grid[i][j];
+            if (grid[i][j] == "1") onCount++;
+        }
+        content += "<br>";
+    }
+
+    document.getElementById("visual6").innerHTML = content;
+    document.getElementById("output6").innerHTML = onCount;
 }
