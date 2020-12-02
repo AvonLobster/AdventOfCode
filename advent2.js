b49f766c48281375488ddb3530a08c38c65c1587
Geoff Carlson <carlson.geoff@gmail.com>
2020-12-02 00:54:13 -0500

    2020Day2Part2

--- advent2.js
+++ advent2.js
@@ -28,8 +28,18 @@ function day2part1() {
 
 function day2part2() {
     let input = document.getElementById("input2").value;
+    let lines = input.split("\n");
+    let validCount = 0;
 
+    for (let i = 0; i < lines.length; i++) {
+        let pos1 = parseInt(lines[i].match(/(\d+)-/));
+        let pos2 = parseInt(lines[i].match(/\d+(?!.*-)/));
+        let ch = lines[i].charAt(lines[i].indexOf(":") - 1);
+        let pw = lines[i].substring(lines[i].indexOf(":") + 2);
 
+        if (pw.charAt(pos1-1) == ch && pw.charAt(pos2-1) != ch) validCount++;
+        else if (pw.charAt(pos1-1) != ch && pw.charAt(pos2-1) == ch) validCount++;
+    }
 
-    document.getElementById("output2").innerHTML = ""
+    document.getElementById("output2").innerHTML = validCount;
 }
