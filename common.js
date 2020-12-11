33e97fc146bf016e8871301102b04647270c87ef
Geoff Carlson <carlson.geoff@gmail.com>
2020-12-10 23:53:47 -0500

    make text area larger

--- common.js
+++ common.js
@@ -1,7 +1,7 @@
 function getHTMLForAdventDay(day, name) {
     let content = '<h2>--- Day <a href="https://adventofcode.com/2020/day/' + day + '" target="_blank">' + day + '</a>: ' + name + ' ---</h2>';
     content += '<button type="button" onclick="insertMyInput(' + day +')">[My Input]</button><br>';
-    content += '<textarea id="input' + day + '"></textarea><br>';
+    content += '<textarea id="input' + day + '" rows="15" cols="80"></textarea><br>';
     content += '<button type="button" onclick="day' + day + 'part1()">[Part 1]</button>';
     content += '<button type="button" onclick="day' + day + 'part2()">[Part 2]</button>';
     content += '<div id="output' + day + '"></div><br>';
