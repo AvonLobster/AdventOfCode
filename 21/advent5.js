85c1d6c473602b14ac1dc831d4d4c6b2043ece15
Geoff Carlson <carlson.geoff@gmail.com>
2021-12-06 06:56:08 -0500

    2021 Day 5 both parts

--- 21/advent5.js
+++ 21/advent5.js
@@ -1,136 +1,181 @@
 function init5() {
-    document.getElementById("adventContent").innerHTML = getHTMLForAdventDay(5, "Binary Boarding");
+    document.getElementById("adventContent").innerHTML = getHTMLForAdventDayAndYear(5, 20121, "Hydrothermal Venture");
 }
+
+class Vent {
+    constructor(coord) {
+        this.x = parseInt(coord.split(",")[0]);
+        this.y = parseInt(coord.split(",")[1]);
+        this.id = coord;
+    }
+}
+
 
 function day5part1() {
     let input = document.getElementById("input5").value;
     let lines = input.split("\n");
-    let maxSeatID = 0;
+    let vents = {};
 
     for (let i = 0; i < lines.length; i++) {
-        let min = 0;
-        let max = 127;
-        let halfL = 64;
-        let row = 0;
-        let col = 0;
+        let coords = lines[i].split(" -> ");
+        let coords1 = new Vent(coords[0]);
+        let coords2 = new Vent(coords[1]);
 
-        if (lines[i].charAt(0) == "F") max -= halfL;
-        else min += halfL;
-        halfL /= 2;
+        if (coords1.x == coords2.x) {
+            if (vents.hasOwnProperty(coords1.id)) {
+                vents[coords1.id]++;
+            } else {
+                vents[coords1.id] = 1;
+            }
 
-        if (lines[i].charAt(1) == "F") max -= halfL;
-        else min += halfL;
-        halfL /= 2;
+            if (vents.hasOwnProperty(coords2.id)) {
+                vents[coords2.id]++;
+            } else {
+                vents[coords2.id] = 1;
+            }
 
-        if (lines[i].charAt(2) == "F") max -= halfL;
-        else min += halfL;
-        halfL /= 2;
+            let minY = Math.min(coords1.y, coords2.y);
+            let maxY = Math.max(coords1.y, coords2.y);
+            minY++;
 
-        if (lines[i].charAt(3) == "F") max -= halfL;
-        else min += halfL;
-        halfL /= 2;
+            for (minY; minY < maxY; minY++) {
+                let id = coords1.x + "," + minY;
+                if (vents.hasOwnProperty(id)) {
+                    vents[id]++;
+                } else {
+                    vents[id] = 1;
+                }
+            }
+            
+        } else if (coords1.y == coords2.y) {
+            if (vents.hasOwnProperty(coords1.id)) {
+                vents[coords1.id]++;
+            } else {
+                vents[coords1.id] = 1;
+            }
 
-        if (lines[i].charAt(4) == "F") max -= halfL;
-        else min += halfL;
-        halfL /= 2;
+            if (vents.hasOwnProperty(coords2.id)) {
+                vents[coords2.id]++;
+            } else {
+                vents[coords2.id] = 1;
+            }
 
-        if (lines[i].charAt(5) == "F") max -= halfL;
-        else min += halfL;
-        halfL /= 2;
+            let minX = Math.min(coords1.x, coords2.x);
+            let maxX = Math.max(coords1.x, coords2.x);
+            minX++;
 
-        if (lines[i].charAt(6) == "F") row = min;
-        else row = max;
-
-        min = 0;
-        max = 7;
-        halfL = 4;
-
-        if (lines[i].charAt(7) == "L") max -= halfL;
-        else min += halfL;
-        halfL /= 2;
-
-        if (lines[i].charAt(8) == "L") max -= halfL;
-        else min += halfL;
-        halfL /= 2;
-
-        if (lines[i].charAt(9) == "L") col = min;
-        else col = max;
-
-        let seatID = row * 8 + col;
-        if (seatID > maxSeatID) maxSeatID = seatID;
+            for (minX; minX < maxX; minX++) {
+                let id = minX + "," + coords1.y;
+                if (vents.hasOwnProperty(id)) {
+                    vents[id]++;
+                } else {
+                    vents[id] = 1;
+                }
+            }
+        }
     }
 
-    document.getElementById("output5").innerHTML = maxSeatID;
+    let sum = 0;
+
+    for (const [key, value] of Object.entries(vents)) {
+        if (value > 1) {
+            sum++;
+        }
+    }
+
+    document.getElementById("output5").innerHTML = sum;
 }
 
 function day5part2() {
     let input = document.getElementById("input5").value;
     let lines = input.split("\n");
-    let seats = Array();
+    let vents = {};
 
     for (let i = 0; i < lines.length; i++) {
-        let min = 0;
-        let max = 127;
-        let halfL = 64;
-        let row = 0;
-        let col = 0;
+        let coords = lines[i].split(" -> ");
+        let coords1 = new Vent(coords[0]);
+        let coords2 = new Vent(coords[1]);
+        let minX = Math.min(coords1.x, coords2.x);
+        let maxX = Math.max(coords1.x, coords2.x);
+        let minY = Math.min(coords1.y, coords2.y);
+        let maxY = Math.max(coords1.y, coords2.y);
 
-        if (lines[i].charAt(0) == "F") max -= halfL;
-        else min += halfL;
-        halfL /= 2;
+        if (vents.hasOwnProperty(coords1.id)) {
+            vents[coords1.id]++;
+        } else {
+            vents[coords1.id] = 1;
+        }
 
-        if (lines[i].charAt(1) == "F") max -= halfL;
-        else min += halfL;
-        halfL /= 2;
+        if (vents.hasOwnProperty(coords2.id)) {
+            vents[coords2.id]++;
+        } else {
+            vents[coords2.id] = 1;
+        }
 
-        if (lines[i].charAt(2) == "F") max -= halfL;
-        else min += halfL;
-        halfL /= 2;
+        // vertical
+        if (coords1.x == coords2.x) {
+            minY++;
 
-        if (lines[i].charAt(3) == "F") max -= halfL;
-        else min += halfL;
-        halfL /= 2;
+            for (minY; minY < maxY; minY++) {
+                let id = coords1.x + "," + minY;
+                if (vents.hasOwnProperty(id)) {
+                    vents[id]++;
+                } else {
+                    vents[id] = 1;
+                }
+            }
+            
+        // horizontal
+        } else if (coords1.y == coords2.y) {
+            minX++;
 
-        if (lines[i].charAt(4) == "F") max -= halfL;
-        else min += halfL;
-        halfL /= 2;
+            for (minX; minX < maxX; minX++) {
+                let id = minX + "," + coords1.y;
+                if (vents.hasOwnProperty(id)) {
+                    vents[id]++;
+                } else {
+                    vents[id] = 1;
+                }
+            }
 
-        if (lines[i].charAt(5) == "F") max -= halfL;
-        else min += halfL;
-        halfL /= 2;
+        // diagonal down
+        } else if ((coords1.x < coords2.x && coords1.y < coords2.y) ||
+            coords1.x > coords2.x && coords1.y > coords2.y) {
+            minX++;
+            minY++;
 
-        if (lines[i].charAt(6) == "F") row = min;
-        else row = max;
+            for (minX; minX < maxX; minX++, minY++) {
+                let id = minX + "," + minY;
+                if (vents.hasOwnProperty(id)) {
+                    vents[id]++;
+                } else {
+                    vents[id] = 1;
+                }
+            }
 
-        min = 0;
-        max = 7;
-        halfL = 4;
+        // diagonal up 0,2 2,0
+        } else {
+            minX++;
+            maxY--;
 
-        if (lines[i].charAt(7) == "L") max -= halfL;
-        else min += halfL;
-        halfL /= 2;
-
-        if (lines[i].charAt(8) == "L") max -= halfL;
-        else min += halfL;
-        halfL /= 2;
-
-        if (lines[i].charAt(9) == "L") col = min;
-        else col = max;
-
-        let seatID = row * 8 + col;
-        seats.push(seatID);
-    }
-
-    seats = seats.sort(function(a, b) {
-        return a - b;
-    });
-
-    let seatMin = seats[0];
-    for (let j = 1; j < seats.length; j++) {
-        if (seats[j] != ++seatMin) {
-            break;
+            for (minX; minX < maxX; minX++, maxY--) {
+                let id = minX + "," + maxY;
+                if (vents.hasOwnProperty(id)) {
+                    vents[id]++;
+                } else {
+                    vents[id] = 1;
+                }
+            }
         }
     }
 
-    document.getElementById("output5").innerHTML = seatMin;
+    let sum = 0;
+
+    for (const [key, value] of Object.entries(vents)) {
+        if (value > 1) {
+            sum++;
+        }
+    }
+
+    document.getElementById("output5").innerHTML = sum;
 }
